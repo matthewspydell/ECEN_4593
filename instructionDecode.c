a3b0dd1cab1e2cf5905310a1b56f24adc145ded9
matthewspydell <matthew.spydell@colorado.edu>
2017-03-13 20:29:18 -0600

    Added instruction masks and pipeline assignments
    Need to create pipeline registers so pipeline assignments can be set correctly.

--- instructionDecode.c
+++ instructionDecode.c
@@ -3,5 +3,35 @@
  */
 
 void instructionDecode(int* instruction) {
-
+  /* R-format instruction masks */
+  int opcodeMask = 0xFC000000;
+  int rsMask = 0x03E00000;
+  int rtMask = 0x001F0000;
+  int rdMask = 0x0000F800;
+  int shamtMask = 0x000007C0;
+  int functMask = 0x0000003F;
+  
+  /* I-format additional instruction mask */
+  int immMask = 0x0000FFFF;
+  
+  /* J-format additional instruction mask */
+  int addMask = 0x03FFFFFF;
+  
+  opcodeRegister = (*instruction & opcodeMask)>>26;
+  
+  /* instruction format is determined by opcode */
+  if (opcodeRegister == 0) {
+    rsRegister = (*instruction & rsMask)>>21;
+    rtRegister = (*instruction & rtMask)>>16;
+    rdRegister = (*instruction & rdMask)>>11;
+    shamt = (*instruction & shamtMask)>>6;
+    funct = (*instruction & functMask);
+  } else if (opcodeRegister == 0x2 || opcodeRegister == 0x3) {
+    address = (*instruction & addMask);  
+  } else {
+    rsRegister = (*instruction & rsMask)>>21;
+    rdRegister = (*instruction & rtMask)>>16; //rd is rt for I-format instructions
+    imm = (*instruction & immMask);
+  }
+  
 }
