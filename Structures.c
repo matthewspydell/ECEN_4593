13935f1b6505df5ecb201326c9e760e6b5633de2
matthewspydell <matthew.spydell@colorado.edu>
2017-03-14 21:17:38 -0600

    minor update

--- Structures.c
+++
@@ -1,7 +0,0 @@
-struct instruction {
-
-   unsigned int opcode =  $;
-   member definition;
-   ...
-   member definition;
-} [one or more structure variables];
---
+++ instructionDecode.h
@@ -0,0 +1,29 @@
+/* @author matthewspydell
+ *
+ */
+#ifndef instructionDecode_h
+#define instructionDecode_h
+
+/* these are the various masks used to pull out each value that needs
+ * to be loaded into the pipeline registers
+ */
+// R-format instruction masks
+uint32_t opcodeMask = 0xFC000000;
+uint32_t rsMask = 0x03E00000;
+uint32_t rtMask = 0x001F0000;
+uint32_t rdMask = 0x0000F800;
+uint32_t shamtMask = 0x000007C0;
+uint32_t functMask = 0x0000003F;
+
+// I-format additional instruction mask
+uint32_t immMask = 0x0000FFFF;
+
+// J-format additional instruction mask
+uint32_t addMask = 0x03FFFFFF;
+
+/* this function decodes the MIPS instruction and
+ * fills in the ID/EX pipeline registers
+ */
+void instructionDecode(int* instruction);
+
+#endif instructionDecode_h
