0a23bf66664c6e5685564027a749aa405fbad151
matthewspydell <matthew.spydell@colorado.edu>
2017-03-14 22:39:38 -0600

    Added instruction fetch function

---
+++ instructionFetch.c
@@ -0,0 +1,9 @@
+/* @author matthewspydell
+ *
+ */
+
+#include "instructionFetch.h"
+
+void instructionFetch(uint32_t* pc) {
+  IF_ID.instructionShadow;
+}
---
+++ instructionFetch.h
@@ -0,0 +1,13 @@
+/* @author matthewspydell
+ *
+ */
+
+#ifndef intsructionFetch_h
+#define intsructionFetch_h
+
+/* this function fetches the instruction from memory that pc is pointing to
+ * and loads the it into the IF/ID pipeline register
+ */
+void instructionFetch(uint32_t* pc);
+
+#endif intsructionFetch_h
