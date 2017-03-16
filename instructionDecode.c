1d8888c25a4cd5a023b3df2ba06c22d2939d0f8a
matthewspydell <matthew.spydell@colorado.edu>
2017-03-16 00:25:35 -0600

    Changed register file and added instruction executions

--- instructionDecode.c
+++ instructionDecode.c
@@ -6,21 +6,20 @@
 
 void instructionDecode() {
 
-  ID_EX.opcodeShadow = (*instruction & opcodeMask)>>26;
+  ID_EX.opcodeShadow = (IF_ID.instruction & opcodeMask)>>26;
 
   // instruction format is determined by opcode
   if (ID_EX.opcodeShadow == 0) { // R-format instruction
-    ID_EX.rsShadow = (*instruction & rsMask)>>21;
-    ID_EX.rtShadow = (*instruction & rtMask)>>16;
-    ID_EX.rdShadow = (*instruction & rdMask)>>11;
-    ID_EX.shamtShadow = (*instruction & shamtMask)>>6;
-    ID_EX.functShadow = (*instruction & functMask);
+    ID_EX.rsShadow = (IF_ID.instruction & rsMask)>>21;
+    ID_EX.rtShadow = (IF_ID.instruction & rtMask)>>16;
+    ID_EX.rdShadow = (IF_ID.instruction & rdMask)>>11;
+    ID_EX.shamtShadow = (IF_ID.instruction & shamtMask)>>6;
+    ID_EX.functShadow = (IF_ID.instruction & functMask);
   } else if (ID_EX.opcodeShadow == 0x2 || ID_EX.opcodeShadow == 0x3) { // J-format instruction
-    ID_EX.addressShadow = (*instruction & addMask);
+    ID_EX.addressShadow = (IF_ID.instruction & addMask);
   } else { // I-format instruction
-    ID_EX.rsShadow = (*instruction & rsMask)>>21;
-    ID_EX.rdShadow = (*instruction & rtMask)>>16; //rd is rt for I-format instructions
-    ID_EX.immShadow = (*instruction & immMask);
+    ID_EX.rsShadow = (IF_ID.instruction & rsMask)>>21;
+    ID_EX.rdShadow = (IF_ID.instruction & rtMask)>>16; //rd is rt for I-format instructions
+    ID_EX.immShadow = (IF_ID.instruction & immMask);
   }
-
 }
--- instructionFetch.c
+++ instructionFetch.c
@@ -5,5 +5,5 @@
 #include "instructionFetch.h"
 
 void instructionFetch(uint32_t* pc) {
-  IF_ID.instructionShadow;
+  IF_ID.instructionShadow = *pc;
 }
